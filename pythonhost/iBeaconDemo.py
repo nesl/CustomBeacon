cdf114fa11895864258d9ed712223e7289eff40d
pdmart <pdmart@gmail.com>
2014-10-16 13:23:05 -0700

    renamed demo script

---
+++ pythonhost/iBeaconDemo.py
@@ -0,0 +1,40 @@
+# --- imports ---
+import time
+from cbeaconpkg.objects import *
+from cbeaconpkg.constants import *
+
+# --- say hello ---
+print()
+print('-------------------------')
+print('      CustomBeacon       ')
+print('-------------------------')
+
+# --- create beacon ---
+ble = BLE112()
+ble.autoSetup()
+
+# --- turn on the green LED to indicate setup is done ---
+ble.setGreenLed(1)
+ble.setRedLed(0)
+	
+# --- set transmit power ---
+ble.setTxPower(TXPOW_LOW)
+
+# --- set ibeacon data ---
+# UUID, major, minor
+ble.setBeaconParams(IBCN_UUID_NESL, IBCN_MAJOR_NESL, 1000)
+
+# --- set advertisement rate ---
+# below gives error 0x0212 - invalid param?
+ble.setAdvRate(10)
+
+# --- enable advertisements ---
+ble.enableAdv()
+
+# --- turn on red LED to indicate advertising ---
+ble.setRedLed(1)
+ble.setGreenLed(0)
+
+# -- close beacon serial --
+#ble.close()
+
